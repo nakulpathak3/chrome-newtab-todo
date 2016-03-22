433b649184f403e549b902ce54c647634a337152
Nakul Pathak <nakulpathak3@hotmail.com>
2016-03-22 19:44:59 -0400

    Consumer key thing works now

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-var secret_object = $.getJSON("js/secrets.json", function(json) {
-  return json;
-});
+var json_req = new XMLHttpRequest();
+json_req.open("GET", "js/secrets.json", false);
+json_req.send();
 
-console.log(secret_object.status);
+consumer_key = JSON.parse(json_req.responseText)["consumer_key"]
+
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.500px.com/v1/users/16754059/galleries/22707459/items/?consumer_key=" + consumer_key, false);
-//xhr.send();
-console.log(xhr.status);
+xhr.send();
 
 var iDiv = document.createElement('div');
 var response_body = JSON.parse(xhr.response);
 iDiv.textContent = response_body["photos"][0]["image_url"];
-//iDiv.textContent = response_body["photos"][0]["image_url"];
 document.getElementsByTagName('body')[0].appendChild(iDiv);
